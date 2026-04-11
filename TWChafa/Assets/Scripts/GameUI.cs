26db1c154f4763f1861d57962f8747378207893b
Celeste Bautista <elicelaena2004@outlook.com>
2026-04-10 19:07:09 -0600

    Examen_SegundoParcial_Celeste

--- TWChafa/Assets/Scripts/GameUI.cs
+++ TWChafa/Assets/Scripts/GameUI.cs
@@ -1,12 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 // Clase de UI que se comunica con el GameManager para comprar torres
 public class GameUI : MonoBehaviour
 {
+    [Header("Referencias de UI")]
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI livesText;
     public Button buyTowerButton;
+
+    [Header("Ajustes de Tienda")]
     public GameObject towerPrefab;
-    public Transform towerParent;
     public int towerCost = 50;
 
     void Start()
@@ -14,17 +19,28 @@ public class GameUI : MonoBehaviour
         buyTowerButton.onClick.AddListener(OnBuyTowerClicked);
     }
 
+    void Update()
+    {
+        if (GameManager.Instance != null)
+        {
+            moneyText.text = "$ " + GameManager.Instance.CurrentMoney;
+            livesText.text = "Vidas: " + GameManager.Instance.CurrentLives;
+        }
+    }
     void OnBuyTowerClicked()
     {
         if (GameManager.Instance.SpendMoney(towerCost))
         {
-            GameObject t = Instantiate(towerPrefab, Vector3.zero, Quaternion.identity);
-            t.transform.SetParent(towerParent);
-            t.transform.localPosition = Vector3.zero;
+            PlaceTower();
         }
         else
         {
             Debug.Log("No hay suficiente dinero para comprar la torre.");
         }
+    }
+
+    void PlaceTower()
+    {
+        Instantiate(towerPrefab, Vector3.zero, Quaternion.identity);
     }
 }
