768ae841e68c83347efdb22fc3657e651a25f634
EmiCelis1 <emsace16@gmail.com>
2026-04-11 23:27:26 -0600

    Update GameUI.cs

--- TWChafa/Assets/Scripts/GameUI.cs
+++ TWChafa/Assets/Scripts/GameUI.cs
@@ -1,30 +1,26 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-// Clase de UI que se comunica con el GameManager para comprar torres
 public class GameUI : MonoBehaviour
 {
     public Button buyTowerButton;
     public GameObject towerPrefab;
-    public Transform towerParent;
     public int towerCost = 50;
 
     void Start()
     {
-        buyTowerButton.onClick.AddListener(OnBuyTowerClicked);
+        buyTowerButton.onClick.AddListener(TryPlaceTower);
+        GameManager.Instance.OnMoneyChanged += UpdateButtonInteractable;
     }
 
-    void OnBuyTowerClicked()
+    void TryPlaceTower()
     {
+        Vector3 gridPos = GridPlacement.GetMouseGridPosition(); // Snap a grid
         if (GameManager.Instance.SpendMoney(towerCost))
         {
-            GameObject t = Instantiate(towerPrefab, Vector3.zero, Quaternion.identity);
-            t.transform.SetParent(towerParent);
-            t.transform.localPosition = Vector3.zero;
-        }
-        else
-        {
-            Debug.Log("No hay suficiente dinero para comprar la torre.");
+            GameObject tower = Instantiate(towerPrefab, gridPos, Quaternion.identity);
         }
     }
+
+    void UpdateButtonInteractable(int currentMoney) => buyTowerButton.interactable = currentMoney >= towerCost;
 }
